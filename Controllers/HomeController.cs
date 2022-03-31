152bc221c890ab81a4f9701fa5ef7550c1815f9d
kamryn-prock <90482936+kamryn-prock@users.noreply.github.com>
2022-03-30 23:21:38 -0600

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MySqlFun.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MySqlFun.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IBowlingRepository _repo { get; set; }
+
+        //Controller
+        public HomeController(IBowlingRepository temp)
+        {
+            _repo = temp;
+        }
+
+        public IActionResult Index()
+        {
+            //create dataset
+            var blah = _repo.Bowlers
+                .ToList();
+                
+
+            return View(blah);
+        }
+
+    }
+}
